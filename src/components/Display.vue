4701dff1d93cf3086597e8813f5c099afd5fa243
senagab <senagab@gmail.com>
2024-11-27 07:55:33 -0400

    feat: componentização css display

--- src/components/Display.vue
+++ src/components/Display.vue
@@ -1,68 +1,67 @@
 <script setup>
-const props = defineProps({
-    valor: {
-        type: String,
-        required: true,
-    },
-    historico: {
-        type: String,
-        required: true,
-    },
-});
+    const props = defineProps({
+        valor: {
+            type: String,
+            required: true,
+        },
+        historico: {
+            type: String,
+            required: true,
+        },
+    });
 </script>
 
 <template>
-    <!-- Display de Histórico -->
     <div class="display">
-
+        <!-- Display de Histórico -->
         <div class="display__last-operation">
             <span class="display__last-operation__text">
-                {{ historico }}
+                {{ historyDisplay }}
             </span>
         </div>
+        <!-- Display Principal -->
+        <input maxlength="21" type="text" class="display__text" :value="estado.valorCorrente" readonly>
     </div>
-    <!-- Display Principal -->
-    <input maxlength="21" type="text" class="display__text" :value="valor" readonly>
 </template>
 
 <style scoped>
-.display {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 20vh;
-    width: auto;
-    background-color: #232323;
-    margin: 30px;
-    border-left: solid 6px #E83D2A;
-    border-radius: 5px;
-    position: relative;
+    .display {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 20vh;
+        width: auto;
+        background-color: #232323;
+        margin: 30px;
+        border-left: solid 6px #E83D2A;
+        border-radius: 5px;
+        position: relative;
 
-    .display__last-operation {
-        position: absolute;
-        top: 20px;
-        right: 15px;
-        color: rgb(150, 150, 150);
-    }
+        .display__last-operation {
+            position: absolute;
+            top: 20px;
+            right: 15px;
+            color: rgb(150, 150, 150);
+        }
 
-    .display__text {
-        width: 500px;
-        height: 60px;
-        background-color: transparent;
-        /* border: solid 1px white; */
-        border: none;
-        position: absolute;
-        bottom: 25px;
-        right: 13px;
-        font-size: 5em;
-        color: white;
-        text-align: right;
-        font-weight: 500;
-        caret-color: transparent;
-    }
+        .display__text {
+            width: 500px;
+            height: 60px;
+            background-color: transparent;
+            /* border: solid 1px white; */
+            border: none;
+            position: absolute;
+            bottom: 25px;
+            right: 13px;
+            font-size: 5em;
+            color: white;
+            text-align: right;
+            font-weight: 500;
+            caret-color: transparent;
+        }
 
-    .display__text:focus {
-        outline: none;
+        .display__text:focus {
+            outline: none;
+        }
     }
-}
 </style>
